577252af8e8c85261ce1b7e206bd73c103019d44
Fitrozi <Fitroziimam22@gmail.com>
2023-04-15 15:02:44 +0700

    homework 7 rakamin

---
+++ soal1.js
@@ -0,0 +1,8 @@
+const hitung = require('./Rumus.js');
+
+
+console.log(`Luas Persegi = ${hitung.luasPersegi(98)}`);
+console.log(`Keliling Persegi = ${hitung.kelilingPersegi(98)}`);
+console.log(`Luas Persegi Panjang = ${hitung.luasPersegiPanjang(13,7)}`);
+console.log(`Keliling Persegi Panjang = ${hitung.kelilingPersegiPanjang(13,7)}`);
+
